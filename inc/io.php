80f35b398966d2f573afd6a982afb286af918bba
GeekyAubergine <c.aubergine@gmail.com>
2016-01-17 17:56:34 +0000

    Sign in and out with google enabled

--- inc/io.php
+++ inc/io.php
@@ -337,13 +337,13 @@ function getUsers($args) {
     $clause = "";
 
     if (isset($args["facebookId"])) {
-      $clause = "where user_facebook_id = " . $args["facebookId"];
+      $clause = "where user_facebook_id = '" . $args["facebookId"] . "'";
     }
     else if (isset($args["googleId"])) {
-      $clause = "where user_google_id = " . $args["googleId"];
+      $clause = "where user_google_id = '" . $args["googleId"] . "'";
     }
     else if (isset($args["twitterId"])) {
-      $clause = "where user_twitter_id = " . $args["twitterId"];
+      $clause = "where user_twitter_id = '" . $args["twitterId"] . "'";
     }
     else if (isset($args["userId"])) {
       $clause = "where user_id = " . $args["userId"];
@@ -395,7 +395,7 @@ function createUser($args) {
     }
 
     $query = "insert into user (user_display_name, user_icon, user_facebook_id, user_google_id, user_twitter_id) values " .
-     "('". $args["displayName"] . "','" . $args["icon"] . "'," . $facebookId . "," . $googleId . "," . $twitterId . ");";
+     "('". $args["displayName"] . "','" . $args["icon"] . "','" . $facebookId . "','" . $googleId . "','" . $twitterId . "');";
 
     $result = $DB->query($query);
 
