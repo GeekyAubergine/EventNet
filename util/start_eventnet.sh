24dbac79ed35e74cf566fa00e6406b900eb71271
GeekyAubergine <c.aubergine@gmail.com>
2016-01-22 14:43:59 +0000

    Added node to start script incase node is installed as node and not nodejs

--- util/start_eventnet.sh
+++ util/start_eventnet.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 pkill php
+pkill node
 pkill nodejs
 
 echo "Starting Node.js server on port 8081"
 
+node server/server.js &
 nodejs server/server.js &
 
 echo "Starting PHP Server on port 8080"
