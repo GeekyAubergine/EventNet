a8f58d2e1291e78d2018b1ecb545ca7e5a045e37
GeekyAubergine <c.aubergine@gmail.com>
2016-02-05 15:18:00 +0000

    Fixed issue with multiple node instances starting when using start.sh

--- util/start_eventnet.sh
+++ util/start_eventnet.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 pkill php
-pkill node
 pkill nodejs
 
 echo "Starting Node.js server on port 8081"
 
-node server/server.js &
 nodejs server/server.js &
 
 echo "Starting PHP Server on port 8080"
