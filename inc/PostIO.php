638997b6d391e3bd8d163d3faf9c6ca95ee2c452
GeekyAubergine <c.aubergine@gmail.com>
2016-03-06 13:21:08 +0000

    Posts now filtered by if user has reported

--- inc/PostIO.php
+++ inc/PostIO.php
@@ -4,29 +4,34 @@ class PostIO {
 
   private $io;
   private $basePostQuery;
-  private $reportsWhereClause;
 
   public function __construct($io) {
     $this->io = $io;
-    $this->basePostQuery = "SELECT post.post_id, post.post_content, post.post_latitude, post.post_longitude, post.post_timestamp, user.user_display_name, user.user_icon, IFNULL(info.number_of_comments, 0) as number_of_comments, reports.number_of_reports as number_of_reports, IF(post.user_id = :userId, 'true', 'false') as posted_by_user FROM post join user using(user_id) LEFT JOIN (select post_id, COUNT(*) AS number_of_comments FROM comment GROUP BY post_id) AS info ON info.post_id = post.post_id LEFT JOIN (select post_id, COUNT(*) AS number_of_reports FROM report GROUP BY post_id) AS reports ON reports.post_id = post.post_id ";
-    $this->reportsWhereClause = " (number_of_reports < :reportsLimit OR number_of_reports IS NULL) ";
+    $this->basePostQuery = "SELECT post.post_id, post.post_content, post.post_latitude, post.post_longitude, post.post_timestamp, user.user_display_name, user.user_icon, IFNULL(info.number_of_comments, 0) as number_of_comments, reports.number_of_reports as number_of_reports, IF(post.user_id = :userId, 'true', 'false') AS posted_by_user FROM post JOIN user USING(user_id) LEFT JOIN report USING(post_id) LEFT JOIN (select post_id, COUNT(*) AS number_of_comments FROM comment GROUP BY post_id) AS info ON info.post_id = post.post_id LEFT JOIN (select post_id, COUNT(*) AS number_of_reports FROM report GROUP BY post_id) AS reports ON reports.post_id = post.post_id ";
   }
 
   public function getPosts($args) {
-    if (isset($args["eventId"])) {
-      $eventId = $args["eventId"];
-    }
-
+    //Get post
     if (isset($args["postId"])) {
       return $this->getPostById($args, $args["postId"]);
     }
 
-    if (isset($args["before"])) {
-      return $this->getPostBeforeTime($args, $eventId, $args["before"]);
-    }
+    //Get posts for event
+    if (isset($args["eventId"])) {
+      $eventId = $args["eventId"];
 
-    if (isset($args["after"])) {
-      return $this->getPostAfterTime($args, $eventId, $args["before"]);
+      $before = "9999-12-31";
+      $after = "1000-01-01";
+
+      if (isset($args["before"])) {
+        $before = $args["before"];
+      }
+
+      if (isset($args["after"])) {
+        $after = $args["after"];
+      }
+
+      return $this->getPostsBetweenDates($args, $eventId, $before, $after);
     }
 
     return $this->getPostsWithEventId($args, $eventId);
@@ -41,31 +46,14 @@ class PostIO {
     return $this->io->queryDB($args, $query, $bindings);
   }
 
-  public function getPostsWithEventId($args, $eventId) {
-    // AND reports.number_of_reports < :reportsLimit ORDER BY post.post_timestamp
-    $query = $this->basePostQuery . "WHERE event_id = :eventId AND " . $this->reportsWhereClause .  "ORDER BY post.post_timestamp asc";
+  private function getPostsBetweenDates($args, $eventId, $before, $after) {
+    $query = $this->basePostQuery . "WHERE event_id = :eventId AND post_timestamp < :before AND post_timestamp > :after AND (number_of_reports < :maxReports OR ISNULL(number_of_reports)) AND IF(report.user_id = :userId, 1, 0) = 0 ORDER BY post.post_timestamp asc ";
     $bindings = [];
     $bindings[":userId"] = $this->io->getUserId($args);
     $bindings[":eventId"] = $eventId;
-    $bindings[":reportsLimit"] = REPORTS_BEFORE_HIDDING_CONTENT;
-
-    return $this->io->queryDB($args, $query, $bindings);
-  }
-
-  private function getPostBeforeTime($args, $eventId, $time) {
-    $query = $this->basePostQuery . "WHERE event_id = :event ORDER BY post.post_timestamp asc ";
-    $bindings = [];
-    $bindings[":eventId"] = $eventId;
-    $bindings[":time"] = $time;
-
-    return $this->io->queryDB($args, $query, $bindings);
-  }
-
-  private function getPostAfterTime($args, $eventId, $time) {
-    $query = $this->basePostQuery . "WHERE event_id = :event ORDER BY post.post_timestamp asc ";
-    $bindings = [];
-    $bindings[":eventId"] = $eventId;
-    $bindings[":time"] = $time;
+    $bindings[":before"] = $before;
+    $bindings[":after"] = $after;
+    $bindings[":maxReports"] = REPORTS_BEFORE_HIDDING_CONTENT;
 
     return $this->io->queryDB($args, $query, $bindings);
   }
