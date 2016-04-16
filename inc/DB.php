96e5bd3f792efcfcb1224cb09ae572f157c11383
Chris Aubert <c.aubergine@gmail.com>
2016-04-16 17:07:33 +0100

    Added comments to DB

--- inc/DB.php
+++ inc/DB.php
@@ -1,19 +1,21 @@
 <?php
+// Class for DB exceptions
 class DBException extends Exception {}
 
+//Class used to perform all DB actions
 class DB {
   private $pdo;
 
+  //Throws a DB exception with given message
   private function throwException($msg = "No error message set") {
-    throw new DBException(
-      $msg//.' '.
-      //$this->$pdo->getCode()
-      // $this->$pdo->getInfo().' '.
-      // $this->$pdo->errorInfo()[1].' '.
-      // $this->$pdo->errorInfo()[2]
-    );
+    throw new DBException($msg);
   }
 
+  /*
+    Constructor.
+    Creates connection to the database, if the database does not exsist then
+    it will attempt to create it and then use it.
+  */
   public function __construct() {
     // CONNECT TO THE DATABASE SERVER
     $dsn = 'mysql:'.DATABASE_HOST.';';
@@ -34,9 +36,11 @@ class DB {
     }
   }
 
+  //Queries the database using the given query and bindings, then returns the results
   public function query($query, $bindings = null) {
     $results = [];
 
+    //Determines if the query is a select query
     $isSelectQuery = strpos(strtolower($query), 'select') !== false;
 
     try {
@@ -47,6 +51,7 @@ class DB {
         $result = $this->pdo->query($query);
       }
 
+      //If it is a select query it fetches the rows, else the result is the row count
       if ($isSelectQuery) {
         $result = $result->fetchAll(PDO::FETCH_ASSOC);
       } else {
@@ -56,6 +61,8 @@ class DB {
        error_log($e);
        $results["meta"]["ok"] = false;
        $results["debug"]["dbException"] = $e->getMessage();
+       $results["data"] = [];
+       return $results;
     }
 
     $results["data"] = $result;
@@ -67,6 +74,7 @@ class DB {
     return $results;
   }
 
+  //Returns the last inserted id to the database
   public function getLastInsertedID() {
     return $this->pdo->lastInsertId();
   }
